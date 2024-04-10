4a78a2ab24725e145e931c6434599b41b29f4abb
annahico <annahico@gmail.com>
2024-04-10 19:46:47 +0200

    aprendiendo inner HTML-TEXT

--- js/kata.js
+++ js/kata.js
@@ -1,5 +1,16 @@
 const boton = document.getElementById("demo");
 
-boton.addEventListener("click", ()=>{
+const parrafo = document.getElementById("principal");
+parrafo.innerText = "Hola Mundo";
+
+
+
+const root = document.getElementById("root");
+root.innerHTML = 
+"<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>";
+
+
+
+boton.addEventListener("click", () => {
     console.log("Hemos hecho click");
 });
