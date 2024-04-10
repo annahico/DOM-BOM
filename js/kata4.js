d9814334308b688ca03021861238f102e43d7272
annahico <annahico@gmail.com>
2024-04-10 21:01:04 +0200

    finalizando kata 4

--- js/kata4.js
+++ js/kata4.js
@@ -3,15 +3,25 @@ const usuarios = ["Bob", "Juan", "Anna", "Marta", "Carlos", "Susana"];
 
 //VARIANTE 1
 
-let usuariosLista = "";
-//     "<li>Bob</li><li>Juan</li><li>Anna</li><li>Marta</li><li>Carlos</li><li>Susana</li>";
+// let usuariosLista = "";
+// //     "<li>Bob</li><li>Juan</li><li>Anna</li><li>Marta</li><li>Carlos</li><li>Susana</li>";
 
 
-for (const usuario of usuarios) {
-    usuariosLista += `<li>${usuario}</li>`;
-}
+// for (const usuario of usuarios) {
+//     usuariosLista += `<li>${usuario}</li>`;
+// }
 
-lista.innerHTML = usuariosLista;  
+// lista.innerHTML = usuariosLista;  
 
 //VARIANTE 2
 
+// let texto = document.createTextNode("Mundo");
+// const parrafo = document.getElementById("parrafo");
+
+// parrafo.appendChild(texto);
+
+usuarios.forEach( (usuario)=>{
+    const item = document.createElement("li");
+    item.textContent = usuario;
+    lista.appendChild(item);
+});
